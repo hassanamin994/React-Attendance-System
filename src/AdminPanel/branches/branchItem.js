5fc7c61a7a9433638f1c1a5c7911564c03a1c6e0
Hassan Amin <hassanmamin994@gmail.com>
2017-05-17 17:06:45 +0200

    branchItem: view tracks in each branch

--- src/AdminPanel/branches/branchItem.js
+++ src/AdminPanel/branches/branchItem.js
@@ -6,11 +6,12 @@ class BranchItem extends Component {
     this.deleteBranch = this.deleteBranch.bind(this)
   }
   render() {
+    let trackNames = this.props.branch.tracks.map(track=> track.name).join()
     return (
       <tr>
         <td>{this.props.branch.name}</td>
         <td>{this.props.branch.city}</td>
-        <td>{this.props.branch.tracks}</td>
+        <td>{trackNames}</td>
         <td><a onClick={this.deleteBranch} className="btn btn-danger btn-xs" href="#">Delete</a> <Link to={"/branches/edit/"+ this.props.branch.id} className="btn btn-primary btn-xs">Edit</Link></td>
       </tr>
     );
